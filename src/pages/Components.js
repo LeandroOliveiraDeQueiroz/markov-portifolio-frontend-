c14f86a1f04c21579373bf6004457e6e69d6a80b
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2021-06-02 19:16:24 +0100

    Create and control screen states

---
+++ src/pages/Components.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  padding: 20px;
+  max-width: 1800px;
+  box-sizing: border-box;
+`;
+
+const Title = styled.h2`
+  margin: 0px 0px 20px 0px;
+`;
+
+const DataGridContainer = styled.div`
+  width: 90%;
+  margin: auto;
+  height: 300px;
+`;
+
+const Painel = styled.div`
+  text-align: left;
+  width: 90%;
+  margin: auto;
+  display: flex;
+`;
+
+const LeftPainel = styled.div`
+  display: flex; // painel em uma linha
+  text-align: left;
+  align-items: center;
+  padding: 10px 0px 0px;
+`;
+
+const RightPainel = styled.div`
+  display: flex;
+  margin-left: auto; // painel em uma linha
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Submit = styled.button`
+   padding: 0.35em 1.2em;
+   border: 0.1em solid #ffffff;
+   border-radius: 0.12em;
+   box-sizing: border-box;
+   text-decoration: none;
+   font-family: "Roboto", sans-serif;
+   font-weight: 300;
+   color: #ffffff;
+  background: rgba(0, 0, 0, 0.7);
+  height: 30px;
+  /* margin: auto unset; */
+  margin-left: 15px;
+`;
+
+export {
+  Container,
+  Title,
+  DataGridContainer,
+  Painel,
+  RightPainel,
+  LeftPainel,
+  Submit,
+};
---
+++ src/pages/Portifolio.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import DeleteIcon from "@material-ui/icons/Delete";
+// import TextField from "@material-ui/core/TextField";
+// import InputLabel from "@material-ui/core/InputLabel";
+import {
+  Select,
+  TextField,
+  InputLabel,
+  MenuItem,
+  Slider,
+} from "@material-ui/core";
+// import MenuItem from "@material-ui/core/MenuItem";
+
+import {
+  Container,
+  Title,
+  DataGridContainer,
+  Painel,
+  RightPainel,
+  LeftPainel,
+  Submit,
+} from "./Components";
+
+const Portifolio = () => {
+  const columns = [
+    {
+      field: "name",
+      headerName: "Ação",
+      type: "string",
+      flex: 0.5,
+    },
+    // {
+    //   field: "risk",
+    //   headerName: "Risco",
+    //   flex: 0.2,
+    // },
+    // {
+    //   field: "dateInit",
+    //   headerName: "Início",
+    //   type: "string",
+    //   flex: 0.2,
+    // },
+    // {
+    //   field: "dateEnd",
+    //   headerName: "Fim",
+    //   type: "string",
+    //   flex: 0.2,
+    // },
+    {
+      field: "weight",
+      headerName: "% Alocada",
+      type: "string",
+      flex: 0.5,
+    },
+  ];
+  const [rows, setRows] = useState([]);
+  const [selectionModel, setSelectionModel] = useState([]);
+  const [checkInDay, setCheckInDay] = useState("");
+  const [checkOutDay, setCheckOutDay] = useState("");
+  const [risk, setRisk] = useState("");
+  const [acao, setAcao] = useState("");
+  const [id, setId] = useState(1);
+
+  const handleClickCheckIn = (e) => {
+    setCheckInDay(e.target.value);
+  };
+
+  const handleClickCheckOut = (e) => {
+    setCheckOutDay(e.target.value);
+  };
+
+  const handleChangeAcao = (e) => {
+    setAcao(e.target.value);
+  };
+
+  const handleChangeRisk = (e, value) => {
+    setRisk(value);
+  };
+
+  const addStock = () => {
+    setRows([
+      ...rows,
+      {
+        id: id,
+        name: acao,
+        // dateInit: checkInDay, dateEnd: checkOutDay
+      },
+    ]);
+    setId(id + 1);
+  };
+
+  const deleteStocks = () => {
+    setRows(
+      rows.filter(({ id }) => {
+        let keep = true;
+        selectionModel.forEach((row) => {
+          if (row === id) {
+            keep = false;
+          }
+        });
+        return keep;
+      })
+    );
+  };
+
+  const portifolio = () => {};
+
+  return (
+    <Container>
+      <Title>Markov Portifolio</Title>
+      <DataGridContainer>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          checkboxSelection
+          onSelectionModelChange={(newSelection) => {
+            setSelectionModel(newSelection.selectionModel);
+          }}
+          selectionModel={selectionModel}
+        />
+      </DataGridContainer>
+      <Painel>
+        <LeftPainel>
+          <DeleteIcon
+            // style={{ margin: "auto unset" }}
+            onClick={deleteStocks}
+          />
+
+          <InputLabel
+            style={{ marginLeft: "15px" }}
+            id="demo-simple-select-label"
+          >
+            Ação
+          </InputLabel>
+          <Select
+            style={{ marginLeft: "15px", minWidth: "160px" }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={acao}
+            onChange={handleChangeAcao}
+          >
+            <MenuItem value={"Ten"}>Ten</MenuItem>
+            <MenuItem value={"Twenty"}>Twenty</MenuItem>
+            <MenuItem value={"Thirty"}>Thirty</MenuItem>
+          </Select>
+          <Submit onClick={addStock}>Adicionar</Submit>
+        </LeftPainel>
+        <RightPainel>
+          <TextField
+            id="date-inicio"
+            label="Início"
+            type="date"
+            value={checkInDay}
+            onChange={handleClickCheckIn}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            style={{ marginLeft: "15px" }}
+            id="date-fim"
+            label="Fim"
+            type="date"
+            value={checkOutDay}
+            onChange={handleClickCheckOut}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <div style={{ marginLeft: "15px" }}>
+            <p>Risco:</p>
+            <Slider
+              value={risk}
+              onChange={handleChangeRisk}
+              valueLabelDisplay="on"
+              style={{ width: "160px" }}
+              min={-100}
+              max={100}
+            />
+          </div>
+          <Submit onClick={portifolio}>Ver Portifólio</Submit>
+        </RightPainel>
+      </Painel>
+    </Container>
+  );
+};
+
+export default Portifolio;
